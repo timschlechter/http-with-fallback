cdf19545bfff999cafb4bbe350254ee857418420
Tim Schlechter <timschlechter@gmail.com>
2013-08-25 20:07:43 +0200

    Code cleanup

--- src/http-with-fallback.js
+++ src/http-with-fallback.js
@@ -3,9 +3,7 @@
 
   angular.module('http-with-fallback', [])
     .factory('httpWithFallback', ['$http', '$q', function($http, $q) {
-      // Clients without local storage will just use $http
-      if (!global.localStorage)
-        return $http;
+
 
       // Constructor function, using $http as prototype
       function HttpWithFallback() { }
@@ -18,11 +16,65 @@
       /**
        * Override $http.get to catch the promise.error
        */
-      httpWithFallback.get = function(url, config) {
-        var deferred = $q.defer(),
-            promise = deferred.promise;
+      httpWithFallback.get = function(url, config) {        
+        // Client doesn't support local storage
+        if (!global.localStorage) {
+          // If no fallback defined, just just $http.get
+          if (!config.fallback) {
+            return $http.get(url, config);
+          }
+          // Local storage won't be used
+          config.dontStoreFallback = true;
+        }
+
+        // Delegate get to $http
+        var deferred = $q.defer();
+        $http.get(url, config)
+        .then(
+          function(response) {
+            // Store in local storage when status === 200
+            if (!response.config.dontStoreFallback && response.status === 200) {
+              localStorage.setItem(url, JSON.stringify({
+                                          data: response.data,
+                                          status: response.status,
+                                          config: response.config,
+                                          headers: response.headers(),
+                                          isFallback: true
+                                        }));
+            }
+            // Resolve with original response
+            deferred.resolve(response);
+          },
+          function(response) {
+            // Try to retrieve from local storage
+            var storedResponse = localStorage.getItem(url);
+            if (storedResponse) {
+              // Data was successfully retrieved from local storage, resolve with status 200
+              storedResponse = JSON.parse(storedResponse);
+              var headers = storedResponse.headers;
+              storedResponse.headers = function() { return headers; };
+              return deferred.resolve(storedResponse);
+            }
+
+            // Try config.fallback
+            if (response.config.fallback) {
+              return deferred.resolve({
+                data: config.fallback,
+                status: 200,
+                headers: response.headers,
+                config: response.config,
+                isFallback: true
+              });
+            }
+
+            // Reject with original error response
+            return deferred.reject(response);
+          }
+        );
 
         // Decorate promise with success and error functions to be compatible with the promise returned by $http.get
+        var promise = deferred.promise;
+
         promise.success = function(fn) {
           promise.then(function(response) {
             fn(response.data, response.status, response.headers, config, response.isFallback);
@@ -36,54 +88,6 @@
           });
           return promise;
         };
-
-        // Delegate get to $http
-        $http.get(url, config)
-             .then( 
-                /* success */
-                function(response) {
-                  deferred.resolve(response);
-
-                  // Store in local storage when status === 200
-                  if (!response.config.dontStoreFallback && response.status === 200) {
-                    localStorage.setItem(url, JSON.stringify({
-                                                data: response.data,
-                                                status: response.status,
-                                                config: response.config,
-                                                headers: response.headers(),
-                                                isFallback: true
-                                              }));
-                  }
-                },
-                /* error */
-                function(response) {
-                  // Try to retrieve from local storage
-                  var storedResponse = localStorage.getItem(url);
-                  if (storedResponse) {
-                    // Data was successfully retrieved from local storage, resolve with status 200
-                    storedResponse = JSON.parse(storedResponse);
-                    var headers = storedResponse.headers;
-                    storedResponse.headers = function() { return headers; };
-                    deferred.resolve(storedResponse);
-                    return;
-                  }
-
-                  // Try config.fallback
-                  if (response.config.fallback) {
-                    deferred.resolve({
-                      data: config.fallback,
-                      status: 200,
-                      headers: response.headers,
-                      config: response.config,
-                      isFallback: true
-                    });
-                    return;
-                  }
-
-                  // Reject with original error response
-                  deferred.reject(response);
-                }
-        );
 
         return promise;
       };
